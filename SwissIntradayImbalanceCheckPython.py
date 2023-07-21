2b503ee23336c31a6eb7c8d641cc5c1156338089
Lukas Dicke <lukas.dicke@statkraft.de>
2023-07-21 16:52:59 +0200

    Versioning of swissgrid schedules missing

--- SwissIntradayImbalanceCheckPython.py
+++ SwissIntradayImbalanceCheckPython.py
@@ -1,3 +1,15 @@
+import json
+import smtplib
+from pathlib import Path
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from email import encoders
+
+#Constants
+#---------------------------------------------------------
+
 SWISSGRID_EIC = "10YCH-SWISSGRIDZ"
 STATKRAFT_EIC = "11XSTATKRAFT001N"
 
@@ -11,11 +23,80 @@ NODE_IN_PARTY = 'InParty'
 NODE_OUT_PARTY = 'OutParty'
 NODE_V = 'v'
 NODE_QTY = 'Qty'
+#---------------------------------------------------------
 
-import json
 
 #Begin of function section
 #---------------------------------------------------------
+
+
+def send_mail(send_from, send_to,send_cc,send_bcc, subject, message, files=[],
+              server="localhost", port=587, username='', password='',
+              use_tls=True):
+    """Compose and send email with provided info and attachments.
+
+    Args:
+        send_from (str): from name
+        send_to (list[str]): to name(s)
+        subject (str): message title
+        message (str): message body
+        files (list[str]): list of file paths to be attached to email
+        server (str): mail server host name
+        port (int): port number
+        username (str): server auth username
+        password (str): server auth password
+        use_tls (bool): use TLS mode
+    """
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+
+    if len(send_to)==1:
+        msg['To'] = send_to
+    else:
+        msg['To'] =", ".join(send_to)
+
+    if len(send_cc)==1:
+        msg['Cc'] = send_cc
+    else:
+        msg['Cc']=", ".join(send_cc)
+
+    if len(send_cc)==1:
+        msg['Bcc']=send_bcc
+    else:
+        msg['Bcc']=", ".join(send_bcc)
+
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message))
+
+    for path in files:
+        part = MIMEBase('application', "octet-stream")
+        with open(path, 'rb') as file:
+            part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename={}'.format(Path(path).name))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server, port)
+    if use_tls:
+        smtp.starttls()
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+    print("Mail sent successfully")
+
+def GetEmailBody(imbalancedPeriods):
+    warnMessage="Hi," + "\n\n" + "please be advised that the current intraday schedule is imabalance in the following periods:" + "\n\n"
+    for key in imbalancedPeriods.keys():
+        print(key)
+        warnMessage=warnMessage + key + "\n"
+
+    warnMessage=warnMessage + "\n\n" + "Kind regards" + "\n\n\n" +"Stakraft Operations"
+
+    return warnMessage
 
 def GetExternalFlowsIntoSwitzerland(scheduleMessage):
     externalFlowsIntoSwitzerland = {}
@@ -165,9 +246,35 @@ for x in range(periods):
         imbalance = True
 
 if imbalance == True:
-    print("Swissgrid schedule is imbalanced in the following periods:")
 
-    for key in imbalancedPeriods.keys():
-        print(key)
+    print("Swissgrid schedule is imbalanced. Email is triggered")
+
+    send_mail(send_from="nominations@statmark.de",
+              send_to=["lukas.dicke@web.de","lukas.dicke@statkraft.de"],
+              send_cc= [],
+              send_bcc= [],
+              subject="Attention: Swissgrid schedule is imbalanced",
+              message=GetEmailBody(imbalancedPeriods),
+              files=[],
+              server= "mail.123domain.eu",
+              port=587,
+              username="nominations@statmark.de",
+              password="fk390krvadf",
+              use_tls=True)
+
+
+    send_mail(send_from="nominations@statkraft.de",
+              send_to=["lukas.dicke@web.de","lukas.dicke@statkraft.de"],
+              send_cc= [],
+              send_bcc= [],
+              subject="Attention: Swissgrid schedule is imbalanced",
+              message=GetEmailBody(imbalancedPeriods),
+              files=[],
+              server= "maildus.energycorp.com",
+              port=25,
+              username="nominations@statkraft.de",
+              password="OlympicGames2018",
+              use_tls=True)
+
 else:
     print("Hooray: Swissgrid schedule is balanced.")
